6cccedb1612b912512afe19ba0a4aacadba12b93
struts2spring <struts2spring@gmail.com>
2016-08-13 17:15:59 +0530

    save

--- src/ui/Main.py
+++ src/ui/Main.py
@@ -4,19 +4,28 @@ Created on 14-Nov-2015
 @author: vijay
 '''
 
-import wx
-from src.ui.view.windowMgr import PyAUIFrame
+'''
+Created on 14-Nov-2015
+@author: vijay
+'''
 
+import wx
+
+
+from src.ui.view.opalview.win import MainFrame
+# 
+global appPath 
 
 class Main():
-
+    
+    def __init__(self):
+        pass
     def myMain(self):
+
         app = wx.App()
-        frame = PyAUIFrame( None)
+        frame = MainFrame(None )
         frame.Show()
         app.MainLoop()
-
-
 if __name__ == '__main__':
     Main().myMain()
-    pass
+pass
