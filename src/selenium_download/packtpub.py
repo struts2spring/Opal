7120bb80bd46534b57ba6a56fd1ce4f13d24f39e
struts2spring <struts2spring@gmail.com>
2016-10-22 23:25:33 +0530

    save

--- src/selenium_download/packtpub.py
+++ src/selenium_download/packtpub.py
@@ -4,6 +4,50 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+import json
+
+
+
+class Book(json.JSONEncoder):
+    def __init__(self, name=None, publisher=None, authors=None, isbn_13=None, datePublished=None, numberOfPages=None,
+                 inLanguage=None, fileSize=None, bookFormat=None, bookDescription=None, bookImgName=None, bookSubTitle=None,
+                 tag=None, subTitle=None):
+        '''
+        Constructor
+        '''
+        self.bookName = name
+        self.subTitle = subTitle
+        self.publisher = publisher
+        
+        self.authors = list()
+        
+        self.isbn_13 = isbn_13
+        self.publishedOn = datePublished
+        self.numberOfPages = numberOfPages
+        self.inLanguage = inLanguage
+        self.fileSize = fileSize
+        self.bookFormat = bookFormat
+        self.subTitle = bookSubTitle
+        self.bookImgName = bookImgName
+        self.tag = tag
+        
+
+    def __str__(self):
+        rep = self.name + self.publisher + self.author + self.isbn_13 + self.datePublished + self.numberOfPages + self.inLanguage + self.fileSize + self.bookFormat + self.tag
+        return rep
+
+class Author(json.JSONEncoder):
+    def __init__(self, authorName='unknown'):
+        '''
+        Constructor
+        '''
+        self.authorName = authorName
+        
+    def __str__(self):
+        rep = self.authorName 
+        return rep
+    
+    
 
 class PacktpubCrawl:
     def __init__(self):
@@ -35,6 +79,9 @@ class PacktpubCrawl:
         try:
             emailEl=driver.find_element_by_css_selector('#packt-user-login-form > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > input:nth-child(1)')
 #             emailEl = driver.find_element_by_name("email")
+            '''
+            Login with user credential
+            '''
             emailEl.send_keys('view7677@gmail.com')
             passwordEl = driver.find_element_by_css_selector("#packt-user-login-form > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > input:nth-child(1)")
             passwordEl.send_keys('default')
@@ -42,12 +89,26 @@ class PacktpubCrawl:
             loginEl.click()
             
             if True:
+                '''
+                clicking on My Account
+                '''
                 myAccountEl=driver.find_element_by_css_selector('#account-bar-logged-in > a:nth-child(1) > div:nth-child(1) > strong:nth-child(1)')
                 myAccountEl.click()
                 
+                '''
+                clicking My ebooks
+                '''
                 myEbook=driver.get(self.baseUrl+'account/my-ebooks')
                 productListEls=driver.find_elements_by_css_selector('div.product-line')
-                print productListEls
+                print len(productListEls)
+                for productEl in productListEls:
+                    print productEl
+                    productEl.click()
+                    readMeEl=productEl.find_element_by_css_selector('.fake-button-text')
+                    readMeEl.click()
+                    print 'new page'
+                    
+#                 product_account_list_el=driver.find_elements_by_css_selector('#product-account-list')
             
             driver.get('https://www.packtpub.com/packt/offers/free-learning')
             try:
@@ -64,7 +125,18 @@ class PacktpubCrawl:
             print 'completed'
         print 'hi'
 
-
+    def createBookDetail(self, bookName=None):
+        book = Book()   
+        book.bookName = "Full Circle "+ bookName
+        book.bookFormat = 'pdf'
+        book.tag = 'Technology'
+        book.inLanguage = 'English'
+        book.subTitle = 'Magazine'
+        book.publisher = "Full Circle"
+        book.bookImgName = bookName + '.jpg'
+        book.hasCover = 'Yes'
+        book.hasCode = 'No'
+        return book
 if __name__ == "__main__":
     PacktpubCrawl().findBookUrl()
     print 'pass'
