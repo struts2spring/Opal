533e60d1c0b162e35e7ef2b2a6d18482a9de420f
DESKTOP-0QIF93L\vijay <struts2spring@gmail.com>
2017-06-08 18:04:45 -0700

    working on refresh

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     description='Opal is a advance book management system.',
     long_description=__doc__,
     packages=['src','src.viewer', 'src.audit','src.viewer.cbr','src.viewer.cbr.images','src.viewer.azw','src.viewer.chm','src.viewer.djvu','src.viewer.epub','src.viewer.mobi'
-              ,'src.dao', 'src.dao.online', 'src.logic', 'src.logic.online', 'src.ui.view.online.thumb', 
+              ,'src.dao', 'src.dao.online', 'src.logic', 'src.logic.online', 'src.ui.view.online.thumb',
               'src.metadata', 'src.selenium_download', 'src.static', 'src.ui', 'src.util',
               'src.ui.view', 'src.ui.view.epub', 'src.ui.view.kivy', 'src.ui.view.metadata', 'src.ui.view.metadata.review',
               'src.ui.view.online', 'src.ui.view.opalview', 'src.images', 'src.ui.view.thumb','src.ui.view.preference'],
@@ -21,7 +21,7 @@ setup(
     platforms='any',
     install_requires=[
 #        'wxPython ==3.0.2.0',
-        'SQLAlchemy == 1.0.12',
+        'SQLAlchemy >= 1.0.12',
         'selenium >= selenium-2.53.2',
         'requests',
 #     	'Kivy==1.9.0',
@@ -30,7 +30,7 @@ setup(
     	'beautifulsoup4==4.4.1',
         'PyPDF2',
         'lxml',
-	'rarfile',
+	    'rarfile',
 #         'python-djvulibre >= 0.7.2'
     ],
     classifiers=[
@@ -40,7 +40,7 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 2.7',
     ],
-    
+
     # What does your project relate to?
     keywords='ebook management ',
     package_data={'src.static': ['*.json'],'src.images':['*.png'],'src.viewer.cbr.images':['*.png']},
